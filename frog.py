9f08e6665ab75679f8b3b9d628d5a88cc4d94265
Justin Lu <justinlu@berkeley.edu>
2017-02-19 06:17:09 -0800

    got image cycling working

--- frog.py
+++ frog.py
@@ -5,26 +5,38 @@ from flask_wtf import Form
 from wtforms.validators import DataRequired
 from wtforms import StringField
 
-img_dir = './static/images/'
 
+img_dir = './static/images/'
+count = 1
 app = Flask(__name__)
 app.secret_key = 'monkey'
 
-class APIRequestForm(Form):
-    input = StringField('Input', validators=[DataRequired()])
 
-@app.route('/')
+class APIRequestForm(Form):
+    SECRET_KEY = "mankey"
+    input = StringField('input', validators=[DataRequired()])
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    form = APIRequestForm()
+    kwargs = {'input': form.input._value()}
+    print(kwargs['input'])
+
     img_path = list(map(lambda path: img_dir + path, os.listdir(img_dir)))
     prev_img = request.cookies.get('prev_img')
-    img = random.sample(img_path, 1)[0]
-    while img == prev_img:
-        img = random.sample(img_path, 1)[0]
+    img = prev_img
 
-    resp = make_response(render_template('image.html', img=img))
-    resp.set_cookie('prev_img', img)
-    return resp
+    if request.method == 'GET':
+        while img == prev_img:
+            img = random.sample(img_path, 1)[0]
+
+        resp = make_response(render_template('image.html', img=img, form=form))
+        resp.set_cookie('prev_img', img)
+        return resp
+    else:
+        return render_template('image.html', img=img, form=form)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
